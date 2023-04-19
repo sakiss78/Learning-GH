ccae66e404b5c4ca85fdf19fc9b73ae375166092
Sakis <athanasios78@yahoo.gr>
2023-04-19 20:01:25 +0300

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Athanasios Kinis on 19/4/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
